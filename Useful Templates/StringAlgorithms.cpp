2ac3dd61d52718c14d3979cff660577fe00b4763
Sarvasva <sarvasva_g@cs.iitr.ac.in>
2025-07-21 13:20:44 +0530

    [StringAlgorithms] Modify the createRollingHash and hashVal functions

--- Useful Templates/StringAlgorithms.cpp
+++ Useful Templates/StringAlgorithms.cpp
@@ -6,11 +6,13 @@ using namespace std;
 /* Time Complexity: O(patternLength)
 Space Complexity: O(patternLength)
 Returns: The prefix pi function */
-vector<int> prefixFunction(string& pattern) {
+vector<int> prefixFunction(string &pattern)
+{
     int patternLength = int(pattern.size());
     vector<int> pi(patternLength + 1);
     pi[0] = pi[1] = 0;
-    for (int i = 1; i < patternLength; i++) {
+    for (int i = 1; i < patternLength; i++)
+    {
         int j = pi[i];
         while (j > 0 && pattern[i] != pattern[j])
             j = pi[j];
@@ -23,14 +25,16 @@ vector<int> prefixFunction(string& pattern) {
 
 /*KMP ALGORITHM*/
 /* Time Complexity: O(n+m) */
-vector<int> occurencesOfPattern(string& pattern, string& text) {
+vector<int> occurencesOfPattern(string &pattern, string &text)
+{
     vector<int> pi = prefixFunction(pattern);
     vector<int> occurenceIndex;
     int textLength = text.size();
     int patternLength = pattern.size();
     int piPointer = 0;
 
-    for (int textPointer = 0; textPointer < textLength; textPointer++) {
+    for (int textPointer = 0; textPointer < textLength; textPointer++)
+    {
         while (piPointer && (piPointer == patternLength || text[textPointer] != pattern[piPointer]))
             piPointer = pi[piPointer];
         if (text[textPointer] == pattern[piPointer])
@@ -42,12 +46,14 @@ vector<int> occurencesOfPattern(string& pattern, string& text) {
 }
 
 /*Z-FUNCTION COMPUTATION*/
-vector<int> zFunction(string s) {
+vector<int> zFunction(string s)
+{
     int n = s.size();
-    vector<int> z(n,0);
+    vector<int> z(n, 0);
     int l = 0, r = 0;
-    for (int i = 1; i < n; i++) {
-        if (i < r) 
+    for (int i = 1; i < n; i++)
+    {
+        if (i < r)
             z[i] = min(r - i, z[i - l]);
         while (i + z[i] < n && s[z[i]] == s[i + z[i]])
             z[i]++;
@@ -60,14 +66,16 @@ vector<int> zFunction(string s) {
 /*ROLLING HASH FUNCTION*/
 /*Hash values are equal => Strings are equal*/
 vector<ll> rollingHash;
-void createRollingHash(string &s){
-    ll len=s.size();
-    rollingHash.assign(len+1,0ll);
-    for(int i=1;i<=len;i++)
-        rollingHash[i]=add(rollingHash[i-1],mul(s[i-1]-'a',binPow(26,i-1)));
+void createRollingHash(string &s)
+{
+    ll len = s.size();
+    rollingHash.assign(len + 1, 0ll);
+    for (int i = 1; i <= len; i++)
+        rollingHash[i] = add(rollingHash[i - 1], mul(s[i - 1] - 'a' + 1, binPow(27, i - 1)));
 }
 
 /*Returns Hash value for the string s[l...r] (0-BASED INDEXING)*/
-ll hashVal(ll l, ll r){
-    return divi(sub(rollingHash[r+1],rollingHash[l]),binPow(26,l));
+ll hashVal(ll l, ll r)
+{
+    return divi(sub(rollingHash[r + 1], rollingHash[l]), binPow(27, l));
 }
